e9ae8d5baa7f841952133128ea3d7eeed96af5ff
mayajanvier <106385182+mayajanvier@users.noreply.github.com>
2022-05-29 17:12:33 +0200

    Update Rot_dataset.py

--- Rotated_MNIST/Rot_dataset.py
+++ Rotated_MNIST/Rot_dataset.py
@@ -1,6 +1,7 @@
 """ Visualization of a rotated image from the train dataset"""
 
 import plotly.express as px
+from Rotated_MNIST.train import train_loader_MNIST
 
 mnist_example = next(iter(train_loader_MNIST))
 fig = px.imshow(mnist_example[0][1][0])
