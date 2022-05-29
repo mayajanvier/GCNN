e8ff69b55341a62a377d6c1d1d88dc13cf1f3c7d
mayajanvier <106385182+mayajanvier@users.noreply.github.com>
2022-05-29 17:11:08 +0200

    Create Z2CNN.py

---
+++ Rotated_MNIST/Models/Z2CNN.py
@@ -0,0 +1,52 @@
+class Z2CNN(nn.Module):
+
+  def __init__( self, input_size: int, input_channels: int, n_feature: int, output_size: int):
+    """
+    :param input_size: number of pixels in the image
+    :param input_channels: number of color channels in the image
+    :param n_feature: size of the hidden dimensions to use
+    :param output_size: expected size of the output
+    """
+    ksize = 3
+    ksize_f = 4
+
+    super().__init__()
+    self.n_feature = n_feature
+    self.conv1 = nn.Conv2d(
+        in_channels=input_channels, out_channels=n_feature, kernel_size=ksize,padding=1
+    )
+    self.bn1 = nn.BatchNorm2d(n_feature)
+    self.conv2 = nn.Conv2d(n_feature, n_feature, kernel_size=ksize,padding=1) #need to add padding to keep the same dimensions of the image 
+    self.bn2 = nn.BatchNorm2d(n_feature)
+    self.conv3 = nn.Conv2d(n_feature, n_feature, kernel_size=ksize,padding=1)
+    self.bn3 = nn.BatchNorm2d(n_feature)
+    self.conv4 = nn.Conv2d(n_feature, n_feature, kernel_size=ksize,padding=1)
+    self.bn4 = nn.BatchNorm2d(n_feature)
+    self.conv5 = nn.Conv2d(n_feature, n_feature, kernel_size=ksize,padding=1)
+    self.bn5 = nn.BatchNorm2d(n_feature)
+    self.conv6 = nn.Conv2d(n_feature, n_feature, kernel_size=ksize,padding=1)
+    self.bn6 = nn.BatchNorm2d(n_feature)
+    self.conv7 = nn.Conv2d(n_feature, output_size, kernel_size=ksize_f,padding=1)
+    #self.bn7 = nn.BatchNorm2d(n_feature)
+    self.pool = nn.MaxPool2d(2, 2)
+    self.dropout2d = nn.Dropout2d(p=0.3)  
+    
+
+  def forward(self, 
+              x: torch.Tensor
+      ) -> torch.Tensor:
+      """
+      :param x: batch of images with size [batch, 1, w, h]
+
+      :returns: predictions with size [batch, output_size]
+      """
+      x = F.relu(self.bn1(self.dropout2d(self.conv1(x))))
+      x = F.relu(self.bn2(self.dropout2d(self.conv2(x))))
+      x = F.max_pool2d(x, kernel_size=2)
+      x = F.relu(self.bn3(self.dropout2d(self.conv3(x))))
+      x = F.relu(self.bn4(self.dropout2d(self.conv4(x))))
+      x = F.relu(self.bn5(self.dropout2d(self.conv5(x))))
+      x = F.relu(self.bn6(self.dropout2d(self.conv6(x))))
+      x = self.conv7(x)
+      x = x.view(x.shape[0],-1) #utiliser softmax ?
+      return x
