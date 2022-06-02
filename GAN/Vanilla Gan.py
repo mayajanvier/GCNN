8badd6c5a114b569679f33a713aabebaad5118fd
mayajanvier <106385182+mayajanvier@users.noreply.github.com>
2022-06-02 16:53:23 +0200

    Update Vanilla Gan.py

--- GAN/Vanilla Gan.py
+++ GAN/Vanilla Gan.py
@@ -74,8 +74,7 @@ class Discriminator(nn.Module):
       
       
 # learning parameters
-#batch_size = 512
-epochs = 2
+epochs = 200
 sample_size = 64 # fixed sample size
 nz = 128 # latent vector size
 k = 1 # number of steps to apply to the discriminator
