db826d4a59208432c27426eb950c15db86f54322
mayajanvier <106385182+mayajanvier@users.noreply.github.com>
2022-06-02 15:21:53 +0200

    Update All_CNN_C.py

--- CIFAR10/All_CNN_C.py
+++ CIFAR10/All_CNN_C.py
@@ -13,8 +13,8 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=2, metavar='N',
-                    help='number of epochs to train (default: 2)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
